8f8076d266729acbb08d09ebae9cbe3e0bee0ccc
Fush1m1 <onyx_1873@icloud.com>
2025-03-22 11:48:04 +0900

    chore: delete cache

---
+++ README.md
@@ -0,0 +1,3 @@
+# sudoku
+
+powered by the compass boilerplate.
---
+++ lib/main.dart
@@ -0,0 +1,52 @@
+// Copyright 2024 Google LLC
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+import 'package:firebase_core/firebase_core.dart';
+import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+import 'package:flutter_animate/flutter_animate.dart';
+import 'package:sudoku/common/utilties.dart';
+import './common/services/navigation.dart';
+import 'package:flutter/foundation.dart';
+
+import './web_setup.dart';
+
+void main() async {
+  Animate.restartOnHotReload = true;
+  if (kIsWeb) loadPicaScript();
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp();
+  runApp(const sudokuApp());
+}
+
+class sudokuApp extends StatelessWidget {
+  const sudokuApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp.router(
+      debugShowCheckedModeBanner: false,
+      scrollBehavior: AppScrollBehavior(),
+      routerConfig: router,
+      title: 'sudoku',
+      theme: ThemeData(
+        colorScheme: ColorScheme.fromSeed(
+          seedColor: const Color(0xff7B4E7F),
+        ),
+        textTheme: GoogleFonts.rubikTextTheme(),
+        useMaterial3: true,
+      ),
+    );
+  }
+}
